d3f561cebbcc2e28d92730700b6dd0613e3bd380
Leonard <Oseghaleleonard39@gmail.com>
2025-11-16 18:39:17 +0100

    Another

--- main.py
+++ main.py
@@ -72,3 +72,60 @@
 # arr = [54, 32, 62, 87, 21, 56, 9, 43]
 # shifted_arr = sort_array(arr, 4)
 # print("Sorted array:", shifted_arr)
+
+
+
+# Given a binary string s, return the number of substrings with all characters 1's. Since the answer may be too large, return it modulo 109 + 7.
+# class Solution:
+#     def numSub(self, s: str) -> int:
+#         sub_strings = s.split("0")
+#         i = 0
+#         for sub_string in sub_strings:
+#             if len(sub_string) > 0:
+#                 d = len(sub_string)
+#                 while d > 0:
+#                     i += d
+#                     d -= 1
+#         return i % (10**9 + 7)
+
+
+
+
+
+# Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+# Symbol       Value
+# I             1
+# V             5
+# X             10
+# L             50
+# C             100
+# D             500
+# M             1000
+# For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+# Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+# I can be placed before V (5) and X (10) to make 4 and 9. 
+# X can be placed before L (50) and C (100) to make 40 and 90. 
+# C can be placed before D (500) and M (1000) to make 400 and 900.
+# Given a roman numeral, convert it to an integer.
+# class Solution:
+#     def romanToInt(self, s: str) -> int:
+#         my_dict = {
+#             "I": 1,
+#             "V": 5,
+#             "X": 10,
+#             "L": 50,
+#             "C": 100,
+#             "D": 500,
+#             "M": 1000
+#         }
+#         my_arr = list(my_dict)
+#         x = 0
+#         for index, element in enumerate(s):
+#             if index < len(s) - 1 and my_dict[element] < my_dict[s[index + 1]]:
+#                 x -= my_dict[element]
+#             else:
+#                 x += my_dict[element]
+#         return x
