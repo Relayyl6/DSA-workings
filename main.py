6f855dc51f24b8e8a1b68ddfb5f236929b11dafd
Leonard <Oseghaleleonard39@gmail.com>
2025-11-16 14:31:03 +0100

    Bubble sort learnt

--- main.py
+++ main.py
@@ -7,28 +7,63 @@
 #     return new_health   
 
 
-def merge_sort(arr):
-    if len(arr) <= 1:
-        return arr
+# merge_sort sorting algorithm
+# def merge_sort(arr):
+#     if len(arr) <= 1:
+#         return arr
 
-    mid = len(arr) // 2
-    left = merge_sort(arr[:mid])
-    right = merge_sort(arr[mid:])
+#     mid = len(arr) // 2
+#     left = merge_sort(arr[:mid])
+#     right = merge_sort(arr[mid:])
 
-    return merge(left, right)
+#     return merge(left, right)
 
-def merge(left, right):
-    result = []
-    while left and right:
-        if left[0] <= right[0]:
-            result.append(left.pop(0))
-        else:
-            result.append(right.pop(0))
-    result.extend(left)
-    result.extend(right)
-    return result
+# def merge(left, right):
+#     result = []
+#     while left and right:
+#         if left[0] <= right[0]:
+#             result.append(left.pop(0))
+#         else:
+#             result.append(right.pop(0))
+#     result.extend(left)
+#     result.extend(right)
+#     return result
+
+# # Example
+# arr = [54, 32, 62, 87, 21, 56, 9, 43]
+# sorted_arr = merge_sort(arr)
+# print("Sorted array:", sorted_arr)
+
+
+
+# # bubble sort algorithm 
+# def bubble_sort(arr):
+#     n = len(arr)
+#     for i in range(n): # for each between 0, 1, 2, 3, 4, 5, 6, 7 ... (length of array( which is n) -1)
+#         for j in range(0, n - i - 1): # compares thm with adjacent elemtnts 
+#             if arr[j] > arr[j + 1]:
+#                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
+#     return arr
+
+# # Example
+# arr = [54, 32, 62, 87, 21, 56, 9, 43]
+# sorted_arr = bubble_sort(arr)
+# print("Sorted array:", sorted_arr)
+
+
+
+# free code camp daily challenge solved. Iterative procedure to coming about answer will be uploaded to a readme later
+def sort_array(arr, k):
+    n = len(arr)
+    if k < 0: # if k is a negative value
+        L = n - (abs(k) % n)
+    else: # if k is a positive shift or shift(k) is greater than the length of the array
+        L = k % n
+
+    new_array = arr[L:] + arr[:L]
+    return new_array
 
 # Example
 arr = [54, 32, 62, 87, 21, 56, 9, 43]
-sorted_arr = merge_sort(arr)
-print("Sorted array:", sorted_arr)
+shifted_arr = sort_array(arr, 4)
+print("Sorted array:", shifted_arr)
